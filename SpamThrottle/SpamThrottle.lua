4bdb8c777d536ec668281bd23a697bd733bef288
Orukxu <bryan.r.davies@gmail.com>
2016-08-20 11:31:58 +0800

    Update SpamThrottle.lua

--- SpamThrottle/SpamThrottle.lua
+++ SpamThrottle/SpamThrottle.lua
@@ -1,7 +1,7 @@
 --[[
 	SpamThrottle - Remove redundant and annoying chat messages
-	Version:	Vanilla 1.11
-	Date:		17 August 2016
+	Version:	Vanilla 1.11a
+	Date:		20 August 2016
 	Author:	Mopar
 	This is a port of SpamThrottle to work with Vanilla WoW, release 1.12.1 and 1.12.2.
 	I am also the author of the retail version (no longer maintained).
